8bc93b84750fd61444524637b0683fdeee653cd1
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-05-05 20:42:02 +0300

    AuthController : 1-Register Endpoint , 2-VerifyOtp Endpoint , 3-ResendOtp Endpoint , 4-Login Endpoint , 5-RefreshSession Endpoint

---
+++ RestflowAPI/RestflowAPI/Controllers/AuthController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using RestflowAPI.DTOs.Auth;
+using RestflowAPI.ServiceInterfaces.Auth;
+
+namespace RestflowAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthController : ControllerBase
+	{
+		private readonly IAuthService _authService;
+
+		public AuthController(IAuthService authService)
+		{
+			_authService = authService;
+		}
+
+		[HttpPost("register")]
+		public async Task<IActionResult> Register([FromBody] RegisterRequestDto request, CancellationToken cancellationToken)
+		{
+			var result = await _authService.RegisterAsync(request, cancellationToken);
+
+			if (!result.IsSuccess)
+			{
+				return BadRequest(result);
+			}
+
+			return Ok(result);
+		}
+		[HttpPost("verify-otp")]
+		public async Task<IActionResult> VerifyOtp([FromBody] VerifyOtpRequestDto request, CancellationToken cancellationToken)
+		{
+			var result = await _authService.VerifyOtpAsync(request, cancellationToken);
+
+			if (!result.IsSuccess)
+			{
+				return BadRequest(result);
+			}
+
+			return Ok(result);
+		}
+		[HttpPost("resend-otp")]
+		public async Task<IActionResult> ResendOtp([FromBody] ResendOtpRequestDto request, CancellationToken cancellationToken)
+		{
+			var result = await _authService.ResendOtpAsync(request, cancellationToken);
+
+			if (!result.IsSuccess)
+			{
+				return BadRequest(result);
+			}
+
+			return Ok(result);
+		}
+		[HttpPost("login")]
+		public async Task<IActionResult> Login([FromBody] LoginRequestDto request, CancellationToken cancellationToken)
+		{
+			var result = await _authService.LoginAsync(request, cancellationToken);
+
+			if (!result.IsSuccess)
+			{
+				return BadRequest(result);
+			}
+
+			return Ok(result);
+		}
+		[HttpPost("refresh-token")]
+		public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenRequestDto request, CancellationToken cancellationToken)
+		{
+			var result = await _authService.RefreshSessionAsync(request, cancellationToken);
+
+			if (!result.IsSuccess)
+			{
+				return BadRequest(result);
+			}
+
+			return Ok(result);
+		}
+	}
+}
