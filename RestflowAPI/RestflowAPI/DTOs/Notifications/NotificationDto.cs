4926391fbf44d1da88622e270bfec3f7f9ce2958
ahmedhasan11 <ahmedelbadawy89910@gmail.com>
2026-06-28 02:07:41 +0300

    INotificationsRepository && NotificationsRepository Implementation

---
+++ RestflowAPI/RestflowAPI/DTOs/Notifications/NotificationDto.cs
@@ -0,0 +1,14 @@
+﻿using RestflowAPI.Enums;
+
+namespace RestflowAPI.DTOs.Notifications
+{
+	public class NotificationDto
+	{
+		public Guid Id { get; set; }
+		public NotificationType Type { get; set; }
+		public string Title { get; set; } = string.Empty;
+		public string Body { get; set; } = string.Empty;
+		public bool IsRead { get; set; }
+		public DateTime CreatedAt { get; set; }
+	}
+}
